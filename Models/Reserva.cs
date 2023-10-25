29d2b3fc1fe4e169a13385c6117098227c2ed2b1
Moisés Santos <moisessantosf@gmail.com>
2023-10-25 03:03:43 -0300

    Update Reserva.cs
    Desafio concluído.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,6 @@
+using System.Drawing;
+using System.Runtime.ConstrainedExecution;
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -17,17 +20,15 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception("Capacidade da suíte excedida!");
             }
         }
-
         public void CadastrarSuite(Suite suite)
         {
             Suite = suite;
@@ -35,23 +36,16 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return this.Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor -= ((valor * 10) / 100);
             }
 
             return valor;
